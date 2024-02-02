6f52401bdd74c138cd0100aca6d9c9b7b2a1c7d9
mykytaterentiev <mykytaterentievua@gmail.com>
2024-02-02 10:54:36 +0100

    g

--- js/1-timer.js
+++ js/1-timer.js
@@ -1,6 +1,6 @@
-import flatpickr from './flatpickr';
+import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import iziToast from './izitoast';
+import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const btnStart = document.querySelector('button');
